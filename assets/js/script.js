69fca35468549282f3fd705e51395e3b857c1d4a
Galang_Yaruna <galangarunayasa@gmail.com>
2025-08-06 15:00:04 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+function btn() {
+    const section = document.getElementById("mySection");
+    const footer = document.getElementById("myfooter");
+
+    
+
+    section.style.display = "flex";
+    footer.style.display = "flex";
+    document.getElementById("mySection").scrollIntoView({ behavior: "smooth" });
+}
+
+function btn1() {
+    const section = document.getElementById("mySection");
+    const footer = document.getElementById("myfooter");
+
+    
+
+    section.style.display = "flex";
+    footer.style.display = "flex";
+    document.getElementById("myfooter").scrollIntoView({ behavior: "smooth" });
+}
